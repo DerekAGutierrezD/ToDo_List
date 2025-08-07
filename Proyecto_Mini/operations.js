154b6841e97a946a8986a4f2b6a0fda6875949f4
Derek Gutierrez <derekgutierrez840@gmail.com>
2025-08-07 16:19:31 -0500

    Add console messages to display tip calculator results – improvement 2

--- Proyecto_Mini/operations.js
+++ Proyecto_Mini/operations.js
@@ -1,13 +1,24 @@
 // Declared variables
-let billValue;
+let billAmount;
 let tipPercentage;
-let tipValue;
-let totalValue;
+let tipAmount;
+let totalToPay;
 
-billValue = 100;                        //Thats bill value
+billAmount = 100;                        //Thats bill value
 tipPercentage = 0.10;                   //This is a tip percentage 
-tipValue = billValue * tipPercentage;   //tip in money 
-totalValue = tipValue + billValue;      //Total bill value
+tipAmount = billAmount * tipPercentage;   //tip in money 
+totalToPay = tipAmount + billAmount;      //Total bill value
 
+console.log(`Your bill amount is: $${billAmount.toFixed(2)}`);
+console.log(`The tip percentage is: ${tipPercentage * 100}%`);
+console.log(`The tip amount is: $${tipAmount.toFixed(2)}`);
+console.log(`Your total amount is: $${totalToPay.toFixed(2)}`)
 
-console.log(`Your bill comes to $${totalValue}, including a $${tipValue} tip.`)
+/*
+console.log('Your bill amount is: $' + billAmount);
+console.log('The tip percentage is: ' + tipPercentage * 100 + '%');
+console.log('The tip amount is: $' + tipAmount);
+console.log('Your total amount is: $' + totalToPay);
+*/
+
+//console.log(`Your bill comes to $${totalToPay}, including a $${tipAmount} tip.`)
