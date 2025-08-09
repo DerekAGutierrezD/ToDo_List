9f78349d63a53319076fcc5c4b535c837ec88973
DerekAGutierrezD <123515433+DerekAGutierrezD@users.noreply.github.com>
2025-08-09 13:20:14 -0500

    Delete Proyecto_Mini1 directory

--- Proyecto_Mini1/Todo_List.js
+++
@@ -1,8 +0,0 @@
-const toDoList = [];
-
-toDoList.push("Walk the dog");
-toDoList.push("Buy bread in the supermarket");
-toDoList.push("Eat pizza with friends in two weeks");
-
-console.log("My ToDo list: ");
-console.log(toDoList);
